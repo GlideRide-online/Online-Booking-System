d34bf33307b7d9ee8e242377bfe8b44a6fdc2f82
akshat2jain <akshat.2024it1031@kiet.edu>
2023-10-17 23:16:03 +0530

    added some styling

--- client/src/Steps/Summary.jsx
+++ client/src/Steps/Summary.jsx
@@ -107,60 +107,81 @@ const Summary = () => {
       ) : (
         <>
           {message.info("Please Check All Your Details before Payment")}
-          <div className="bg-gray-100 p-4 md:p-8 rounded-lg shadow-md grid grid-cols-1 md:grid-cols-2 gap-4">
-            <h2 className="text-2xl font-semibold mb-4">Summary</h2>
+          <div className="bg-gray-100 font-bold p-4 md:p-8 rounded-lg shadow-md grid grid-cols-1 md:grid-cols-2 gap-4">
+            <h2 className="text-3xl font-semibold mb-4">Summary</h2>
             <div className="mb-4">
-              <h3 className="text-lg font-medium">Service Selected:</h3>
-              <p className="text-gray-700">{state.step1Data.name}</p>
+              <h3 className="text-lg font-medium font-mono text-blue-600">
+                Service Selected:
+              </h3>
+              <p className="text-gray-700 font-serif">{state.step1Data.name}</p>
             </div>
             <div>
-              <h3 className="text-lg font-medium">Date:</h3>
-              <p className="text-gray-700">
+              <h3 className="text-lg font-medium font-mono text-blue-600">
+                Date:
+              </h3>
+              <p className="text-gray-700 font-serif">
                 {formatDate(state.step2Data.date)}
               </p>
             </div>
             <div className="mb-4">
-              <h3 className="text-lg font-medium">Time Slot:</h3>
-              <p className="text-gray-700">
+              <h3 className="text-lg font-medium font-mono text-blue-600">
+                Time Slot:
+              </h3>
+              <p className="text-gray-700 font-serif">
                 {state.step2Data.timeSlot.start} to{" "}
                 {state.step2Data.timeSlot.end}
               </p>
             </div>
             <div className="mb-4">
-              <h3 className="text-lg font-medium">Personal Details:</h3>
-              <p className="text-gray-700">Name: {state.step3Data.name}</p>
-              <p className="text-gray-700">Email: {state.step3Data.email}</p>
-              <p className="text-gray-700">
+              <h3 className="text-lg font-medium font-mono text-blue-600">
+                Personal Details:
+              </h3>
+              <p className="text-gray-700 font-serif">
+                Name: {state.step3Data.name}
+              </p>
+              <p className="text-gray-700 font-serif">
+                Email: {state.step3Data.email}
+              </p>
+              <p className="text-gray-700 font-serif">
                 Phone Number: {state.step3Data.phoneNumber}
               </p>
-              <p className="text-gray-700">Gender: {state.step3Data.gender}</p>
-              <p className="text-gray-700">Age: {state.step3Data.age}</p>
-              <p className="text-gray-700">
+              <p className="text-gray-700 font-serif">
+                Gender: {state.step3Data.gender}
+              </p>
+              <p className="text-gray-700 font-serif">
+                Age: {state.step3Data.age}
+              </p>
+              <p className="text-gray-700 font-serif">
                 College Name: {state.step3Data.collegeName}
               </p>
-              <p className="text-gray-700">
+              <p className="text-gray-700 font-serif">
                 College Year: {state.step3Data.collegeYear}
               </p>
             </div>
             <div>
-              <h3 className="text-lg font-medium">Amount to Be Paid</h3>
+              <h3 className="text-lg font-medium font-mono text-blue-600">
+                Amount to Be Paid
+              </h3>
               <p className="text-gray-700 pt-2">
                 Price - {state.step1Data.price}
               </p>
               <p className="text-gray-700 pt-2">
-                To be Paid Now(securtiy) - {state.step1Data.securtiy}
+                To be Paid Now(security) - {state.step1Data.securtiy}
               </p>
             </div>
             {/* <div>
               <h3 className="text-lg font-medium">Coupan Code</h3>
               <CoupanCode />
             </div> */}
-            <div className="mt-6">
+            <div className="mt-6 grid gap-1">
               <button
-                className="bg-blue-500 hover:bg-blue-600 text-white px-6 py-2 rounded-md font-semibold transition duration-300"
+                className="bg-blue-500 font-mono hover:bg-blue-600 text-white px-6 py-2 rounded-md font-semibold transition duration-300"
                 onClick={handlePayment}
               >
-                Pay Now
+                Pay Now {state.step1Data.securtiy}
+              </button>
+              <button className="bg-blue-500 font-mono hover:bg-blue-600 text-white px-6 py-2 rounded-md font-semibold transition duration-300">
+                Fill Details Again
               </button>
             </div>
           </div>
